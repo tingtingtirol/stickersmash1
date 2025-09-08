49110e5d199ab776f163a652f88b1ae4d850993b
Rimuru <yancemagoncia2@gmail.com>
2025-09-08 18:24:56 +0800

    saved

---
+++ app/tabs/index.tsx
@@ -0,0 +1,31 @@
+import { Text, View,  StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Sticker Smash</Text>
+      <Link href="/" style={styles.button}>
+        Go to About screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+});
